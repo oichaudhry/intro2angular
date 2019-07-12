2724baf1ce2c191d819817285d2c0addf375a471
Omar-Chaudhry <oichaudh@uci.edu>
2019-07-11 21:43:27 -0700

    Decorations - introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 function log(target, name, descriptor) {
   console.log(target,name,descriptor)
+  // Store original function in a variable
+  const original = descriptor.value
+  // do some manipulation with a descriptor.value
+  descriptor.value = function() {
+    console.log("This function was hacked!")
+  }
+  // return the descriptor
+  return descriptor
 }
 
 
@@ -13,6 +21,10 @@ function log(target, name, descriptor) {
 })
 export class AppComponent {
   title = 'intro2angular';
+
+  constructor() {
+    this.aSimpleMethod()
+  }
 
   @log
   aSimpleMethod(){
